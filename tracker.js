7c1690901af11c75674640ff1957018b88f2b382
viparthasarathy <viparthasarathy@gmail.com>
2016-09-20 11:53:16 -0700

    Refactor utility.js into prototype based logic

--- tracker.js
+++ tracker.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 const bencode = require('bencode');
 const http = require('http');
 const Buffer = require('buffer').Buffer;
-const utility = require('./utility');
+const Utility = require('./utility');
 
 function Tracker(params) {
   this.torrent = bencode.decode(fs.readFileSync(params.fileName));
-  this.infoHash = utility.generateInfoHash(this.torrent.info);
+  this.infoHash = Utility.generateInfoHash(this.torrent.info);
   this.totalSize = this.calculateTotalSize();
-  this.peerID = utility.generatePeerID();
+  this.peerID = Utility.generatePeerID();
   this.downloaded = 0;
   this.uploaded = 0;
   this.port = 6881;
 }
 
 Tracker.prototype.requestPeers = function() {
-}
+};
 
 Tracker.prototype.buildAnnounceRequest = function() {
   const announceURL = this.torrent.announce.toString('utf8');
@@ -27,10 +27,10 @@ Tracker.prototype.buildAnnounceRequest = function() {
               '&downloaded=' + this.downloaded +
               '&left=' + (this.totalSize - this.downloaded)
   return announceURL + query;
-}
+};
 
 Tracker.prototype.calculateTotalSize = function() {
   return this.torrent.info.files ? this.torrent.info.files.map( file => file.length ).reduce( (a, b) => a + b ) : this.torrent.info.length;
-}
+};
 
 module.exports = Tracker;
--- utility.js
+++ utility.js
@@ -1,16 +1,18 @@
+'use strict';
 const crypto = require('crypto');
 const bencode = require('bencode');
 
-function generatePeerID() {
+function Utility() {}
+
+Utility.prototype.generatePeerID = function() {
   const id = crypto.randomBytes(20);
   Buffer.from('-VP0001-').copy(id, 0);
   return id;
-}
+};
 
-function generateInfoHash(torrentInfo) {
+Utility.prototype.generateInfoHash = function(torrentInfo) {
   const info = bencode.encode(torrentInfo);
   return crypto.createHash('sha1').update(info).digest();
-}
+};
 
-module.exports.generatePeerID = generatePeerID;
-module.exports.generateInfoHash = generateInfoHash;
+module.exports = new Utility();
